d87e90e03700b4abf80f7f89d852c420b464ad36
Jeff Davis <jeff.davis@gmail.com>
2011-10-28 07:04:50 -0400

    pull pallette out of debugging

--- debugging.go
+++ debugging.go
@@ -27,6 +27,20 @@ func (ih ImageHelper) At(x, y int) image.Color {
 	return ih.pixel(y/4, x/4)
 }
 
+
+//implement Image for fancy image debugging
+func (m *Map) ColorModel() image.ColorModel {
+	return image.NRGBAColorModel
+}
+func (m *Map) Bounds() image.Rectangle {
+	return image.Rect(0, 0, m.Cols*4, m.Rows*4)
+}
+func (m *Map) At(x, y int) image.NRGBAColor {
+	loc := m.ToLocation(Point{y, x})
+	return m.Grid[loc].Color()
+}
+
+
 func (m *Map) WriteDebugImage(Desc string, seq int, At func(row, col int) image.NRGBAColor) {
 
 	//use -imgprefix="bot0" to make a series of images (bot0.0.png ... bot0.N.png) which
@@ -51,50 +65,4 @@ func (m *Map) WriteDebugImage(Desc string, seq int, At func(row, col int) image.
 	if err != nil {
 		log.Panicf("Couldn't encode png (%s)", err)
 	}
-}
-
-func (o Item) Color() image.NRGBAColor {
-	switch o {
-	case UNKNOWN:
-		return image.NRGBAColor{0xb0, 0xb0, 0xb0, 0xff}
-	case WATER:
-		return image.NRGBAColor{0x10, 0x10, 0x50, 0xff}
-	case FOOD:
-		return image.NRGBAColor{0xe0, 0xe0, 0xc0, 0xff}
-	case LAND:
-		return image.NRGBAColor{0x8b, 0x45, 0x13, 0xff}
-	case MY_ANT:
-		return image.NRGBAColor{0xf0, 0x00, 0x00, 0xff}
-	case PLAYER1:
-		return image.NRGBAColor{0xf0, 0xf0, 0x00, 0xff}
-	case PLAYER2:
-		return image.NRGBAColor{0x00, 0xf0, 0x00, 0xff}
-	case PLAYER3:
-		return image.NRGBAColor{0x00, 0x00, 0xf0, 0xff}
-	case PLAYER4:
-		return image.NRGBAColor{0xf0, 0x00, 0xf0, 0xff}
-	case PLAYER5:
-		return image.NRGBAColor{0xf0, 0xf0, 0xf0, 0xff}
-	case PLAYER6:
-		return image.NRGBAColor{0x80, 0x80, 0x00, 0xff}
-	case PLAYER7:
-		return image.NRGBAColor{0x00, 0x80, 0x80, 0xff}
-	case PLAYER8:
-		return image.NRGBAColor{0x80, 0x00, 0x80, 0xff}
-	case PLAYER9:
-		return image.NRGBAColor{0x80, 0x00, 0xf0, 0xff}
-	}
-	return image.NRGBAColor{0xff, 0xff, 0xff, 0xff}
-}
-
-//implement Image for fancy image debugging
-func (m *Map) ColorModel() image.ColorModel {
-	return image.NRGBAColorModel
-}
-func (m *Map) Bounds() image.Rectangle {
-	return image.Rect(0, 0, m.Cols*4, m.Rows*4)
-}
-func (m *Map) At(x, y int) image.NRGBAColor {
-	loc := m.ToLocation(Point{y, x})
-	return m.Grid[loc].Color()
 }
