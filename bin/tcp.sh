dcc6ccab27cd79fc5681ea6b38619ab8aed4b514
Jeff Davis <jeff.davis@gmail.com>
2011-11-03 19:25:14 -0400

    tcp.sh changes

--- bin/tcp.sh
+++ bin/tcp.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 ROOT=~/src/ai/bot
-BOT=bugnutsv5
+BOT=bugnutsv6
+RBOT=bugnutsv5
 BHOST=ants.fluxid.pl
 ARCH="`uname -s`"
 DATE="`date +%Y%m%d-%H%M`"
 LOG=$ROOT/log/tcp/${BHOST}.log.$DATE
 ERR=$ROOT/log/tcp/${BHOST}.err.$DATE
-EXE=$ROOT/bin/$ARCH/$BOT 
+EXE=$ROOT/bin/$ARCH/${BOT}
 if [ ! -x $EXE ]; then 
     echo "No executable found for $BOT in $EXE"
     exit 1
 fi
-python $ROOT/bin/tcpclient.py $BHOST 2081 $EXE $BOT donuts -1 > $LOG 2> $ERR
+while [ 1 ] ; do 
+    echo "INFO: $EXE started at `date` LOG $LOG"
+    python $ROOT/bin/tcpclient.py $BHOST 2081 $EXE $RBOT donuts -1 >> $LOG 2>> $ERR
+    echo "INFO: failed at `date`"
+sleep 900
+done
