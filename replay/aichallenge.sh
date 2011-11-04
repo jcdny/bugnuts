92ec64d5dad50669c121a06f8351f5217b23d00c
Jeff Davis <jeff.davis@gmail.com>
2011-11-04 13:13:31 -0400

    fix it so it remembers last game gotten

--- replay/aichallenge.sh
+++ replay/aichallenge.sh
@@ -1,36 +1,50 @@
 #!/bin/bash
-GAME=49000
-END=52000
 HOST=aichallenge.org
-BASE=http://aichallenge.org/game/0/
+URLBASE=http://aichallenge.org/game/0/
 ROOT=~/src/ai/bot/replay
+SUFFIX=".replaygz"
+PREFIX=""
+LAST=$ROOT/data/LAST.$HOST
+
 if [ ! -d $ROOT/data ]; then
     echo "Fatal $ROOT/data does not exist"
     exit 1
 fi
 
 cd $ROOT/data
+
 mkdir -p $HOST || exit 1
 
+GAME="`cat $LAST || echo 1`"
+END=$1
+
+if [ "$END" = "" -o "$GAME" = "" ]; then
+    echo "FATAL: game range unkown end: $END start: $GAME"
+    exit 1
+fi
+
 DATE="`date +%Y%m%d-%H%M`"
-LOG="${HOST}.log.$DATE"
-ERR="${HOST}.err.$DATE"
+LOG="log/log.${HOST}.$DATE"
+ERR="log/err.${HOST}.$DATE"
 echo "INFO: getting $HOST games $GAME to $END log $LOG"
 exec > $LOG 2> $ERR
 
 cd $HOST
+
 while [ $GAME -lt $END ]; do
     D="`expr $GAME / 1000`"
-
-    DEST=$D/$GAME.replaygz
-    SOURCE=${BASE}$D/$GAME.replaygz
+    mkdir -p $D
+    FILENAME="$PREFIX$GAME$SUFFIX"
+    DEST="$D/$FILENAME"
+    SOURCE=${URLBASE}$DEST
     if [ -f $DEST ]; then
         echo "INFO: found game $DEST"
     else
-        echo "INFO: getting $SOURCE"
+        echo "INFO: getting $SOURCE to `pwd`/$DEST"
         curl --create-dirs -o $DEST $SOURCE || echo Ouch
-        ls -1l $DEST | grep gz
-        sleep `expr $RANDOM % 8 + 4`
+        ls -1l $DEST
+        sleep `expr $RANDOM % 10 + 10`
     fi
     GAME="`expr $GAME + 1`"
+    echo $GAME > $LAST
 done
