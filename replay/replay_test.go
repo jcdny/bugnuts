05b7506eebbf3f43b82b29992780293cbe5f26e0
Jeff Davis <jeff.davis@gmail.com>
2011-11-08 11:10:36 -0500

    add a benchmark for replay parse

--- replay/replay_test.go
+++ replay/replay_test.go
@@ -21,4 +21,20 @@ func TestReplayLoad(t *testing.T) {
 	if err != nil || m.ReplayFormat != "json" {
 		t.Errorf("Error on Unmarshal %v, format found was %s", err, m.ReplayFormat)
 	}
+
+	log.Printf("%#v", m)
+}
+
+func BenchmarkReplayLoad(b *testing.B) {
+
+	buf, err := ioutil.ReadFile("testdata/replay.0.json")
+	if err != nil {
+		log.Panicf("Readfile error %v", err)
+	}
+
+	// Do the actual parse here
+	for i := 0; i < b.N; i++ {
+		m := &Match{}
+		err = json.Unmarshal(buf, m)
+	}
 }
