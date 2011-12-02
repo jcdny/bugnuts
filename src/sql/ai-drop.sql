16c019899d5e320c14603ec550317808ee1c1f1f
Jeff Davis <jeff.davis@gmail.com>
2011-12-01 22:30:54 -0500

    datamodel for game data

---
+++ src/sql/ai-drop.sql
@@ -0,0 +1,11 @@
+DROP TABLE gameplayer;
+DROP TABLE games;
+DROP SEQUENCE games_gid_seq;
+DROP TABLE versions;
+DROP TABLE maps;
+DROP SEQUENCE maps_mid_seq;
+DROP SEQUENCE versions_vid_seq;
+DROP TABLE players;
+DROP SEQUENCE players_pid_seq;
+DROP TABLE servers;
+DROP SEQUENCE servers_sid_seq;
