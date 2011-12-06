0a96ec229636c574a2ef35ecfe6ab8bcae4c1331
Jeff Davis <jeff.davis@gmail.com>
2011-12-05 23:39:23 -0500

    skeleton for combat

--- src/pkg/bot8/combat.go
+++ src/pkg/bot8/combat.go
@@ -14,12 +14,42 @@ import (
 	. "bugnuts/viz"
 )
 
-func Combat(endants) {
+type AntMove struct {
+	Start    Location
+	End      Location
+	NStep    int
+	Steps    [8]Direction
+	Prefered Direction
+}
+
+type AntPartition struct {
+	Ants [][]AntMove
+}
+
+func Combat(s *State, ants []*AntStep) {
 	// compute ants which are in combat range of original or new locations.
+	cants := s.InCombat()
 
 	// partition by connectedness
+	sants, pants := s.Partition(ants, cants)
+
+	// Compute available time
+
+	// Compute per partition time budget
 
 	// sim to compute best moves
+	for {
+		for _, ap := range pants {
+			if time.NanoSeconds()+ap.estSimTime > s.TurnEnd {
+				break
+			}
+			s.Sim(ap)
+		}
+	}
 
-	// vis call for help
+	// Move combat moves back to antstep
+
+	// vis
+
+	// call for help
 }
