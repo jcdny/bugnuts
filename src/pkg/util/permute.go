d58bf999018de34336fd8d8c57fe6468e1d640c6
Jeff Davis <jeff.davis@gmail.com>
2011-12-13 17:40:01 -0500

    generate permutes of arbitrary sets

---
+++ src/pkg/util/permute.go
@@ -0,0 +1,41 @@
+package util
+
+func Permutations(n, states int) [][]int {
+	nl := make([]int, n)
+	for i := range nl {
+		nl[i] = states
+	}
+	return PermuteList(nl)
+}
+
+// PermuteList takes a list of state counts and generates the permutations...
+func PermuteList(N []int) [][]int {
+
+	n := len(N)
+	cnt := make([]int, n)
+
+	nperm := 1
+	for i := range N {
+		nperm *= N[i]
+	}
+
+	out := make([][]int, nperm)
+	buf := make([]int, nperm*n)
+	for c, i := 0, 0; i < nperm; i++ {
+		out[i] = buf[i*n : (i+1)*n]
+		for j := 0; j < n; j++ {
+			if c > 0 {
+				cnt[j] += 1
+				c--
+			}
+			if cnt[j] == N[j] {
+				cnt[j] = 0
+				c = 1
+			}
+		}
+		c = 1
+		copy(out[i], cnt)
+	}
+
+	return out
+}
