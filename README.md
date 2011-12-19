5eb18a012b782cc385dd59b5473fef271e31ea1c
Jeff Davis <jeff.davis@gmail.com>
2011-12-19 18:35:17 -0500

    start in on documenting code

--- README.md
+++ README.md
@@ -4,14 +4,34 @@ My code for the [AI Challege 2011](http://aichallenge.org)
 
 A Go learning experience...
 
-Clone the git repo here - `git clone git://github.com/jcdny/bugnuts.git`
+The code lives at https://github.com/jcdny/bugnuts
 
-## Commentary
+The branch ``submission'' contains the code that was actually
+submitted for the contest including the file submission.zip which is
+the version uploaded to aichallenge.org.
 
-See docs/Log.md and docs/Ideas.md
+The master branch is a cleaned up version of that code with more
+comments and some of the copy/paste idiocy I inflicted on myself
+removed.  Also includes final maps.
 
-## Status
+# Code Roadmap
 
-Bot is too stupid for own good.
+The executable is built in _src/cmd/bot_, the packages are in _src/pkg_.
+The _botN_ packages are particular versions of the Bot (and _statbot_ is a
+logicless bot which supports taking in the true gamestate from the
+replay engine), movement logic resides mostly in Bot<N>, _replay_ and
+_engine_ are the packages for implementing replay parsing. Symmetry
+analysis lives partly in maps and partly in torus.  State handles game
+state updating, metrics, and statistics. watcher contains timers and
+the watch point evaluation.
+
+There is a fancy replay analyzer that lives in _src/cmd/analyze_ that I 
+mostly wrote to learn how to use channels.
+
+
+
+
+
+
 
 
---
+++ tools/replay/bhickey.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+HOST=bhickey.net
+URLBASE=http://bhickey.net:2080/replay
+ROOT=~/tcp
+LAST=$ROOT/data/$HOST/LAST
+LOCK=$ROOT/data/$HOST/LOCK
+STOP=$ROOT/data/$HOST/STOP
+
+if [ ! -d $ROOT/data ]; then
+    echo "Fatal $ROOT/data does not exist"
+    exit 1
+fi
+cd $ROOT/data
+mkdir -p log || exit 1
+mkdir -p $HOST || exit 1
+
+if [ -f $LOCK ]; then
+    echo "LOCK for $HOST exists.  remove $LOCK to run"
+    exit 1
+fi
+echo `date` > $HOST/LOCK
+
+GAME="`cat $LAST || echo 1`"
+END=450
+#"`curl -s http://$HOST | egrep "href='/replay.[0-9]" | head -n1 | sed -e 's#^.*/replay\.\([0-9]*\)[^0-9].*#\1#'`"
+
+if [ "$END" = "" -o "$GAME" = "" ]; then
+    echo "FATAL: game range error end: \"$END\" start: \"$GAME\""
+    exit 1
+fi
+if [ "$GAME" -gt "$END" ]; then
+    echo "FATAL: end before start, End: \"$END\" Start: \"$GAME\""
+    exit 1
+fi
+
+DATE="`date +%Y%m%d-%H%M`"
+LOG="log/log.${HOST}.$DATE"
+ERR="log/err.${HOST}.$DATE"
+
+echo "INFO: getting $HOST games $GAME to $END log $LOG"
+exec > $LOG 2> $ERR
+echo "INFO: getting $HOST games $GAME to $END"
+
+cd $HOST
+
+while [ $GAME -lt $END -a ! -f $STOP ]; do
+    D="`expr $GAME / 1000`"
+        mkdir -p $D
+    DEST="$D/replay.$GAME"
+    SOURCE=${URLBASE}.$GAME
+    if [ -f $D/replay.$GAME -o -f $D/replay.${GAME}.gz ]; then
+        echo "INFO: found game $DEST"
+    else
+        echo "INFO: getting $SOURCE"
+        curl --create-dirs -o $D/replay.$GAME ${URLBASE}.$GAME || echo Ouch
+        if [ -s $DEST ]; then
+            gzip $DEST
+            chmod 444 ${DEST}.gz
+            ls -1l ${DEST}.gz
+        fi
+        curl -o /dev/null -d "email=jeff.davis@gmail.com&stat=$HOST:game&value=$GAME" http://api.stathat.com/ez &
+        curl -o /dev/null -d "email=jeff.davis@gmail.com&stat=$HOST:downloaded&count=1" http://api.stathat.com/ez &
+        sleep `expr $RANDOM % 15 + 15`
+    fi
+    GAME="`expr $GAME + 1`"
+    echo $GAME > $LAST
+done
+rm -f $LOCK
+
